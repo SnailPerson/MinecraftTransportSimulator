04126b4d62c9224b59ced2defad45a4957c6e8af
SnailPerson <97041048+SnailPerson@users.noreply.github.com>
2026-05-14 23:29:43 +0200

    Update AEntityD_Definable.java

--- mccore/src/main/java/minecrafttransportsimulator/entities/components/AEntityD_Definable.java
+++ mccore/src/main/java/minecrafttransportsimulator/entities/components/AEntityD_Definable.java
@@ -1005,6 +1005,8 @@ public abstract class AEntityD_Definable<JSONDefinition extends AJSONMultiModelP
                 return new ComputedVariable(this, variable, partialTicks -> InterfaceManager.clientInterface.getClientPlayer().getPosition().subtract(position).getAngles(true).z, false);
             case ("config_simplethrottle"):
                 return new ComputedVariable(this, variable, partialTicks -> ConfigSystem.client.controlSettings.simpleThrottle.value ? 1 : 0, false);
+			case ("config_arcade"):
+                return new ComputedVariable(this, variable, partialTicks -> ConfigSystem.client.controlSettings.arcadeMode.value ? 1 : 0, false);
             case ("config_innerwindows"):
                 return new ComputedVariable(this, variable, partialTicks -> ConfigSystem.client.renderingSettings.innerWindows.value ? 1 : 0, false);
             default: {
